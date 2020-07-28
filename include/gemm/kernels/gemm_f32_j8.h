710216ebde6d7099fa781b9bf1d3ae826e4f976e
Malith <malithjayaweera.d@northeastern.edu>
2020-07-28 14:44:19 -0400

    added GEMM kernels and tests

---
+++ include/gemm/kernels/gemm_f32_j8.h
@@ -0,0 +1,93 @@
+#ifndef __GEMM_F32_J8_H_
+#define __GEMM_F32_J8_H_
+
+#include <immintrin.h>
+#include <memory.h>
+
+#include <iostream>
+
+#include "../../types/types.h"
+
+index_t gemm_f32_j8(char transa, char transb, index_t m, index_t n, index_t k,
+                    float alpha, float* a, index_t lda, float* b, index_t ldb,
+                    float beta, float* c, index_t ldc) {
+  uint32_t arr_a[16];
+  for (uint32_t i = 0; i < 16; ++i) {
+    arr_a[i] = i * k;
+  }
+
+  uint32_t arr_c[16];
+  for (uint32_t i = 0; i < 16; ++i) {
+    arr_c[i] = i * n;
+  }
+  __m512 zmm0 = _mm512_setzero_ps();
+  __m512i zmm1 = _mm512_loadu_si512(&arr_c);
+
+  // load m rows
+  uint16_t mask = ~(0xffff << m);
+
+  float* c_ptr = c;
+  __m512 czmm16 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, c_ptr, 0x4);
+  c_ptr++;
+  __m512 czmm17 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, c_ptr, 0x4);
+  c_ptr++;
+  __m512 czmm18 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, c_ptr, 0x4);
+  c_ptr++;
+  __m512 czmm19 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, c_ptr, 0x4);
+  c_ptr++;
+  __m512 czmm20 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, c_ptr, 0x4);
+  c_ptr++;
+  __m512 czmm21 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, c_ptr, 0x4);
+  c_ptr++;
+  __m512 czmm22 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, c_ptr, 0x4);
+  c_ptr++;
+  __m512 czmm23 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, c_ptr, 0x4);
+
+  zmm1 = _mm512_loadu_si512(&arr_a);
+  for (index_t kk = 0; kk < k; kk += 1) {
+    float* a_ptr = a + kk;
+    __m512 azmm0 = _mm512_mask_i32gather_ps(zmm0, mask, zmm1, a_ptr, 0x4);
+
+    // load n B columns
+    float* b_ptr = b + kk * n;
+    __m512 bzmm1 = _mm512_set1_ps(*b_ptr);
+    __m512 bzmm2 = _mm512_set1_ps(*(b_ptr + 1));
+    __m512 bzmm3 = _mm512_set1_ps(*(b_ptr + 2));
+    __m512 bzmm4 = _mm512_set1_ps(*(b_ptr + 3));
+    __m512 bzmm5 = _mm512_set1_ps(*(b_ptr + 4));
+    __m512 bzmm6 = _mm512_set1_ps(*(b_ptr + 5));
+    __m512 bzmm7 = _mm512_set1_ps(*(b_ptr + 6));
+    __m512 bzmm8 = _mm512_set1_ps(*(b_ptr + 7));
+    __m512 bzmm9 = _mm512_set1_ps(*(b_ptr + 8));
+
+    czmm16 = _mm512_fmadd_ps(azmm0, bzmm1, czmm16);
+    czmm17 = _mm512_fmadd_ps(azmm0, bzmm2, czmm17);
+    czmm18 = _mm512_fmadd_ps(azmm0, bzmm3, czmm18);
+    czmm19 = _mm512_fmadd_ps(azmm0, bzmm4, czmm19);
+    czmm20 = _mm512_fmadd_ps(azmm0, bzmm5, czmm20);
+    czmm21 = _mm512_fmadd_ps(azmm0, bzmm6, czmm21);
+    czmm22 = _mm512_fmadd_ps(azmm0, bzmm7, czmm22);
+    czmm23 = _mm512_fmadd_ps(azmm0, bzmm8, czmm23);
+  }
+
+  zmm1 = _mm512_loadu_si512(&arr_c);
+
+  c_ptr = c;
+  _mm512_mask_i32scatter_ps(c_ptr, mask, zmm1, czmm16, 0x4);
+  c_ptr++;
+  _mm512_mask_i32scatter_ps(c_ptr, mask, zmm1, czmm17, 0x4);
+  c_ptr++;
+  _mm512_mask_i32scatter_ps(c_ptr, mask, zmm1, czmm18, 0x4);
+  c_ptr++;
+  _mm512_mask_i32scatter_ps(c_ptr, mask, zmm1, czmm19, 0x4);
+  c_ptr++;
+  _mm512_mask_i32scatter_ps(c_ptr, mask, zmm1, czmm20, 0x4);
+  c_ptr++;
+  _mm512_mask_i32scatter_ps(c_ptr, mask, zmm1, czmm21, 0x4);
+  c_ptr++;
+  _mm512_mask_i32scatter_ps(c_ptr, mask, zmm1, czmm22, 0x4);
+  c_ptr++;
+  _mm512_mask_i32scatter_ps(c_ptr, mask, zmm1, czmm23, 0x4);
+}
+
+#endif
--- test/gemm/test_gemm_kernel.cc
+++ test/gemm/test_gemm_kernel.cc
@@ -1,13 +1,355 @@
 #include <chrono>
 
 #include "gemm/gemm.h"
+#include "gemm/kernels/gemm_f32_j1.h"
 #include "gemm/kernels/gemm_f32_j10.h"
 #include "gemm/kernels/gemm_f32_j11.h"
 #include "gemm/kernels/gemm_f32_j12.h"
 #include "gemm/kernels/gemm_f32_j13.h"
 #include "gemm/kernels/gemm_f32_j14.h"
 #include "gemm/kernels/gemm_f32_j15.h"
+#include "gemm/kernels/gemm_f32_j2.h"
+#include "gemm/kernels/gemm_f32_j3.h"
+#include "gemm/kernels/gemm_f32_j4.h"
+#include "gemm/kernels/gemm_f32_j5.h"
+#include "gemm/kernels/gemm_f32_j6.h"
+#include "gemm/kernels/gemm_f32_j7.h"
+#include "gemm/kernels/gemm_f32_j8.h"
+#include "gemm/kernels/gemm_f32_j9.h"
 #include "gtest/gtest.h"
+
+TEST(GEMM, f32_j1) {
+  index_t m = 15;
+  index_t n = 1;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j1('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
+
+TEST(GEMM, f32_j2) {
+  index_t m = 12;
+  index_t n = 2;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j2('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
+
+TEST(GEMM, f32_j3) {
+  index_t m = 12;
+  index_t n = 3;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j3('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
+
+TEST(GEMM, f32_j4) {
+  index_t m = 12;
+  index_t n = 4;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j4('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
+
+TEST(GEMM, f32_j5) {
+  index_t m = 12;
+  index_t n = 5;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j5('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
+
+TEST(GEMM, f32_j6) {
+  index_t m = 12;
+  index_t n = 6;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j6('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
+
+TEST(GEMM, f32_j7) {
+  index_t m = 12;
+  index_t n = 7;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j7('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
+
+TEST(GEMM, f32_j8) {
+  index_t m = 12;
+  index_t n = 8;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j8('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
+
+TEST(GEMM, f32_j9) {
+  index_t m = 12;
+  index_t n = 9;
+  index_t k = 10;
+
+  float *A = static_cast<float *>(std::malloc(m * k * sizeof(float)));
+  float *B = static_cast<float *>(std::malloc(n * k * sizeof(float)));
+  float *C = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+  float *C_REF = static_cast<float *>(std::malloc(m * n * sizeof(float)));
+
+  for (index_t i = 0; i < m; ++i) {
+    for (index_t j = 0; j < k; ++j) {
+      A[i * k + j] = i * k + j + 1;
+    }
+  }
+  for (index_t i = 0; i < k; ++i) {
+    for (index_t j = 0; j < n; ++j) {
+      index_t idx = i * n + j;
+      B[i * n + j] = idx + 1;
+    }
+  }
+
+  memset(C, 0, m * n * sizeof(float));
+  memset(C_REF, 0, m * n * sizeof(float));
+  gemm<float>('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C_REF, n);
+  gemm_f32_j9('N', 'N', m, n, k, 1.0, A, k, B, n, 0, C, n);
+
+  for (index_t i = 0; i < m * n; ++i) {
+    EXPECT_EQ(C_REF[i], C[i]);
+  }
+
+  std::free(A);
+  std::free(B);
+  std::free(C);
+  std::free(C_REF);
+}
 
 TEST(GEMM, f32_j10) {
   index_t m = 10;
