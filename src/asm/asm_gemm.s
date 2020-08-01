a0f38a04d29d058a0df1defa57c0b0290cae24de
Malith <malithjayaweera.d@northeastern.edu>
2020-08-01 11:37:05 -0400

    matrix multiplication loop to the assembly function

--- src/asm/asm_gemm.s
+++ src/asm/asm_gemm.s
@@ -30,12 +30,14 @@ asm_gemm:
     pushq %rbp              
     movq %rsp, %rbp
     # ALLOCATE STACK SPACE
-    subq $64, %rsp                           # [STACK ALLOCATE 64 BYTES]
+    subq $0x80, %rsp                         # [STACK ALLOCATE 128 BYTES]
     # FUNCTION BODY
     vxorps %zmm0, %zmm0, %zmm0               # SET SRC REGISTER TO ZERO
     movq 0x10(%rbp), %r10                    # GATHER INDICES
     vmovdqu32 (%r10), %zmm1                  # LD C MATRIX INDICES [DSTRYBL:r10]
-    movq %rdx, %r10                          # [SAVE C MATRIX PTR TO r10]
+    movq %rdx, -0x8(%rbp)                    # [SAVE C MATRIX PTR TO STACK]
+    movq %r12, -0x10(%rbp)                   # [SAVE R12 REG TO STACK]
+    movq %r13, -0x18(%rbp)                   # [SAVE R13 REG TO STACK]
 
     vmovaps %zmm0, %zmm2                     # COPY SRC REGISTER TO FUTURE C0 
     movzwl 0x18(%rbp), %r11d                 # LD MASK FROM STACK  [SCRBL: r11]
@@ -97,13 +99,38 @@ asm_gemm:
     vmovaps %zmm0, %zmm16                    # COPY SRC REGISTER TO FUTURE C0 
     kmovw  %r11d, %k1                        # SET MASK
     vgatherdps (%rdx, %zmm1, 4), %zmm16{%k1} # LOAD C14 COL TO REGISTER
-    vmovups %zmm16, (%rdx)                   # TEST LINE: REMOVE (STORE C)
 
     vmovdqu32 (%r9), %zmm1                   # LD A MATRIX INDICES
-
+    # START MATRIX MULTIPLICATION
+    xorl %eax, %eax                          # INDEX i
+    movq 0x20(%rbp), %r10                    # ID
+.LOOPBEGIN:
+    # LOOP INIT
+    cmpl %edi, %eax                          # TEST i < k
+    jnb .LOOPEXIT
+    # LOOP INIT END
+    kmovw  %r11d, %k1                        # SET MASK
+    vgatherdps (%rsi, %zmm1, 4), %zmm0{%k1}  # LOAD A COL TO REGISTER
+                                             # SET MATRIX B
+    lea (%r8, %r10, 0x4), %r12               # OFFSET
+    movq (%r12), %r12
+    movq %rcx, %r13                          # MOV BASE PAGE ADDRESS
+    addq %r12, %r13                          # CALLABLE ADDRESS
+    callq *%r13                              # CALL FUNCTION TO SET MAT B
+    # LOOP CLEANUP
+    lea 0x4(%rsi), %rsi                      # INCREMENT A MATRIX PTR
+    addl $1, %eax                            # i = i + 1
+    addl $1, %r10d                           # ID = ID + 1
+    jmp .LOOPBEGIN
+    # LOOP CLEANUP END
+.LOOPEXIT:
+    vmovups %zmm25, (%rdx)                   # TEST LINE: REMOVE (STORE C)
     # FUNCTION BODY ENDS
     # DEALLOCATE STACK SPACE
-    addq $64, %rsp
+    movq %r12, %rax
+    movq  -0x10(%rbp), %r12                   # [SAVE R12 REG FROM STACK]
+    movq  -0x18(%rbp), %r13                   # [SAVE R13 REG FROM STACK]
+    addq $0x80, %rsp
     # FUNCTION EPILOG BEGINS HERE
     movq %rbp, %rsp        
     pop %rbp
