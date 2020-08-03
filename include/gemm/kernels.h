18494198b6267390ddfb0aa9fb80d2e510ef5a5a
Malith <malithjayaweera.d@northeastern.edu>
2020-08-03 08:21:24 -0400

    reorganized header files

---
+++ include/gemm/kernels.h
@@ -0,0 +1,20 @@
+#ifndef __KERNELS_H_
+#define __KERNELS_H_
+
+#include "./kernels/gemm_f32_j1.h"
+#include "./kernels/gemm_f32_j10.h"
+#include "./kernels/gemm_f32_j11.h"
+#include "./kernels/gemm_f32_j12.h"
+#include "./kernels/gemm_f32_j13.h"
+#include "./kernels/gemm_f32_j14.h"
+#include "./kernels/gemm_f32_j15.h"
+#include "./kernels/gemm_f32_j2.h"
+#include "./kernels/gemm_f32_j3.h"
+#include "./kernels/gemm_f32_j4.h"
+#include "./kernels/gemm_f32_j5.h"
+#include "./kernels/gemm_f32_j6.h"
+#include "./kernels/gemm_f32_j7.h"
+#include "./kernels/gemm_f32_j8.h"
+#include "./kernels/gemm_f32_j9.h"
+
+#endif
