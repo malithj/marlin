96f9d44b3d106d89c1e91c9bc815c687a80a2676
Malith <malithjayaweera.d@northeastern.edu>
2020-08-03 08:22:08 -0400

    replaced header file

--- test/jit/test_jit_kernel.cc
+++ test/jit/test_jit_kernel.cc
@@ -1,69 +1,9 @@
 #include <chrono>
 
+#include "gemm/asm_kernels.h"
 #include "gemm/gemm.h"
 #include "gtest/gtest.h"
 #include "jit/jitter.h"
-
-extern "C" index_t asm_gemm_f32_j1(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j2(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j3(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j4(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j5(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j6(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j7(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j8(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j9(index_t k, float *a, float *c, void *p_addr,
-                                   index_t *offset_data, uint32_t *arr_a,
-                                   uint32_t *arr_c, uint16_t mask, index_t idx);
-
-extern "C" index_t asm_gemm_f32_j10(index_t k, float *a, float *c, void *p_addr,
-                                    index_t *offset_data, uint32_t *arr_a,
-                                    uint32_t *arr_c, uint16_t mask,
-                                    index_t idx);
-
-extern "C" index_t asm_gemm_f32_j11(index_t k, float *a, float *c, void *p_addr,
-                                    index_t *offset_data, uint32_t *arr_a,
-                                    uint32_t *arr_c, uint16_t mask,
-                                    index_t idx);
-
-extern "C" index_t asm_gemm_f32_j12(index_t k, float *a, float *c, void *p_addr,
-                                    index_t *offset_data, uint32_t *arr_a,
-                                    uint32_t *arr_c, uint16_t mask,
-                                    index_t idx);
-
-extern "C" index_t asm_gemm_f32_j13(index_t k, float *a, float *c, void *p_addr,
-                                    index_t *offset_data, uint32_t *arr_a,
-                                    uint32_t *arr_c, uint16_t mask,
-                                    index_t idx);
-
-extern "C" index_t asm_gemm_f32_j14(index_t k, float *a, float *c, void *p_addr,
-                                    index_t *offset_data, uint32_t *arr_a,
-                                    uint32_t *arr_c, uint16_t mask,
-                                    index_t idx);
 
 TEST(ASM_GEMM, f32_j1) {
   constexpr index_t m = 16;
