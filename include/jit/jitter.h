4a61527af64b25cbec29b682c11b00a121c1c2a3
Malith <malithjayaweera.d@northeastern.edu>
2020-08-06 08:34:23 -0400

    removed JIT overhead

--- include/jit/jitter.h
+++ include/jit/jitter.h
@@ -76,12 +76,12 @@ class Jitter {
 
 template <typename T>
 Jitter<T>::~Jitter() {
-  if (this->arr_a_offsets != nullptr) {
-    delete[] arr_a_offsets;
-  }
-  if (this->arr_c_offsets != nullptr) {
-    delete[] arr_c_offsets;
-  }
+  // if (this->arr_a_offsets != nullptr) {
+  //   delete[] arr_a_offsets;
+  // }
+  // if (this->arr_c_offsets != nullptr) {
+  //   delete[] arr_c_offsets;
+  // }
 }
 
 template <typename T>
@@ -103,17 +103,17 @@ void Jitter<T>::generate_code(T* matrix, int m, int k, int n) {
   this->p_addr = codelet->get_p_addr();
   this->page_size_bytes = codelet->get_page_size_bytes();
   this->offset_data = this->bytecode->get_offset_buffer()->mutable_data();
-  this->arr_a_offsets = new uint32_t[16];
-  this->arr_c_offsets = new uint32_t[16];
+  // this->arr_a_offsets = new uint32_t[16];
+  // this->arr_c_offsets = new uint32_t[16];
   this->mask = 0xffff;
   this->pmask = ~(0xffff << (m & 0xf));
 
-  for (uint32_t i = 0; i < 16; ++i) {
-    arr_a_offsets[i] = i * k;
-  }
-  for (uint32_t i = 0; i < 16; ++i) {
-    arr_c_offsets[i] = i * n;
-  }
+  // for (uint32_t i = 0; i < 16; ++i) {
+  //   arr_a_offsets[i] = i * k;
+  // }
+  // for (uint32_t i = 0; i < 16; ++i) {
+  //   arr_c_offsets[i] = i * n;
+  // }
 }
 
 template <typename T>
