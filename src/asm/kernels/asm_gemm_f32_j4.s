17310d4198a24064c54a884591534989319db5b8
Malith <malithjayaweera.d@northeastern.edu>
2020-08-06 08:25:14 -0400

    modified to column major format

--- src/asm/kernels/asm_gemm_f32_j4.s
+++ src/asm/kernels/asm_gemm_f32_j4.s
@@ -6,20 +6,20 @@
 # 4 B COLS AND 1 A COLS.
 #
 # CALLING SEQUENCE IS AS FOLLOWS:
-#       RDI : K
-#       RSI : MATRIX A PTR
-#       RDX : MATRIX C PTR
-#       RCX : BASE PAGE ADDRESS
-#       R8  : PAGE OFFSET DATA
-#       R9  : MATRIX A GATHER INDICES
-# 0x10(EBP) : MATRIX C GATHER INDICES
+#       RDI : M
+#       RSI : K
+#       RDX : N
+#       RCX : MATRIX A PTR
+#       R8  : MATRIX C PTR
+#       R9  : BASE PAGE ADDRESS
+# 0x10(EBP) : PAGE OFFSET DATA
 # 0x18(EBP) : MASK
 # 0x20(EBP) : B MATRIX IDX (CODE GEN INVOKER IDX)
 #
 # FUNCTION DEFINITION TO BE DECLARED IS AS FOLLOWS:
-#       extern "C" index_t asm_gemm_f32_j4(index_t k, float* a, float* c,
-#                                          void* p_addr, index_t* offset_data, 
-#                                          uint32_t* arr_a, uint32_t* arr_c, 
+#       extern "C" index_t asm_gemm_f32_j4(index_t m, index_t k, index_t n,
+#                                          float *a, float *c, void *p_addr,
+#                                          index_t *offset_data, 
 #                                          uint16_t mask, index_t idx)
 #                        
 #
@@ -33,10 +33,7 @@ asm_gemm_f32_j4:
     # ALLOCATE STACK SPACE
     subq $0x28, %rsp                         # [STACK ALLOCATE 32 BYTES]
     # FUNCTION BODY
-    vxorps %zmm0, %zmm0, %zmm0               # SET SRC REGISTER TO ZERO
-    movq 0x10(%rbp), %r10                    # GATHER INDICES
-    vmovdqu32 (%r10), %zmm1                  # LD C MATRIX INDICES [DSTRYBL:r10]
-    movq %rdx, -0x8(%rbp)                    # [SAVE C MATRIX PTR TO STACK]
+    movq %r8, -0x8(%rbp)                     # [SAVE C MATRIX PTR TO STACK]
     movq %r12, -0x10(%rbp)                   # [SAVE R12 REG TO STACK]
     movq %r13, -0x18(%rbp)                   # [SAVE R13 REG TO STACK]
     movq %r14, -0x20(%rbp)                   # [SAVE R14 REG TO STACK]
@@ -48,22 +45,21 @@ asm_gemm_f32_j4:
     vxorps %zmm4, %zmm4, %zmm4
     vxorps %zmm5, %zmm5, %zmm5
 
-    vmovdqu32 (%r9), %zmm1                   # LD A MATRIX INDICES
     # START MATRIX MULTIPLICATION
     xorl %eax, %eax                          # INDEX i
     movq 0x20(%rbp), %r10                    # ID
+    movq 0x10(%rbp), %r8                     # PAGE OFFSET DATA
 .LOOPBEGIN:
     # LOOP INIT
-    cmpl %edi, %eax                          # TEST i < k
+    cmpl %esi, %eax                          # TEST i < k
     jnb .LOOPEXIT
     # LOOP INIT END
-    kmovw  %r11d, %k1                        # SET MASK
-    vgatherdps (%rsi, %zmm1, 4), %zmm0{%k1}  # LOAD A COL TO REGISTER
-                                             # SET MATRIX B
+    vmovups (%rcx), %zmm0{%k1}               # LOAD A COL TO REGISTER
+    # SET MATRIX B
     movl %eax, %r14d                         # CALLER SAVE
+    movq %r9, %r13                           # MOV BASE PAGE ADDRESS
     lea (%r8, %r10, 0x8), %r12               # OFFSET
     movq (%r12), %r12
-    movq %rcx, %r13                          # MOV BASE PAGE ADDRESS
     addq %r12, %r13                          # CALLABLE ADDRESS
     callq *%r13                              # CALL FUNCTION TO SET MAT B
     movl %r14d, %eax                         # CALLER RESTORE
@@ -72,29 +68,21 @@ asm_gemm_f32_j4:
     vfmadd231ps %zmm0, %zmm30, %zmm3         # z3 += z0 * z30
     vfmadd231ps %zmm0, %zmm29, %zmm4         # z4 += z0 * z29
     vfmadd231ps %zmm0, %zmm28, %zmm5         # z5 += z0 * z28
-    # LOOP CLEANUP
-    lea 0x4(%rsi), %rsi                      # INCREMENT A MATRIX PTR
+    # LOOP CLEANUP                   
+    lea (%rcx, %rdi, 0x4), %rcx              # INCREMENT A MATRIX PTR
     addl $1, %eax                            # i = i + 1
     addl $1, %r10d                           # ID = ID + 1
     jmp .LOOPBEGIN
     # LOOP CLEANUP END
 .LOOPEXIT:
-    vxorps %zmm0, %zmm0, %zmm0               # SET SRC REGISTER TO ZERO
     movq -0x8(%rbp), %rdx                    # [RESTORE C MATRIX PTR TO STACK]
-    # vmovups %zmm2, (%rdx)                    # TEST LINE: REMOVE (STORE C)
-    movq 0x10(%rbp), %r10                    # GATHER INDICES
-    vmovdqu32 (%r10), %zmm1                  # LD C MATRIX INDICES [DSTRYBL:r10]
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm2, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm3, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm4, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm5, (%rdx, %zmm1, 0x4){%k1} 
+    vmovups %zmm2, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm3, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm4, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm5, (%rdx){%k1}
     movq $0x1, %rax                          # SET RETURN VALUE
     # FUNCTION BODY ENDS
     # DEALLOCATE STACK SPACE
--- src/asm/kernels/asm_gemm_f32_j8.s
+++ src/asm/kernels/asm_gemm_f32_j8.s
@@ -6,20 +6,20 @@
 # 8 B COLS AND 1 A COLS.
 #
 # CALLING SEQUENCE IS AS FOLLOWS:
-#       RDI : K
-#       RSI : MATRIX A PTR
-#       RDX : MATRIX C PTR
-#       RCX : BASE PAGE ADDRESS
-#       R8  : PAGE OFFSET DATA
-#       R9  : MATRIX A GATHER INDICES
-# 0x10(EBP) : MATRIX C GATHER INDICES
+#       RDI : M
+#       RSI : K
+#       RDX : N
+#       RCX : MATRIX A PTR
+#       R8  : MATRIX C PTR
+#       R9  : BASE PAGE ADDRESS
+# 0x10(EBP) : PAGE OFFSET DATA
 # 0x18(EBP) : MASK
 # 0x20(EBP) : B MATRIX IDX (CODE GEN INVOKER IDX)
 #
 # FUNCTION DEFINITION TO BE DECLARED IS AS FOLLOWS:
-#       extern "C" index_t asm_gemm_f32_j8(index_t k, float* a, float* c,
-#                                          void* p_addr, index_t* offset_data, 
-#                                          uint32_t* arr_a, uint32_t* arr_c, 
+#       extern "C" index_t asm_gemm_f32_j8(index_t m, index_t k, index_t n,
+#                                          float *a, float *c, void *p_addr,
+#                                          index_t *offset_data, 
 #                                          uint16_t mask, index_t idx)
 #                        
 #
@@ -33,10 +33,7 @@ asm_gemm_f32_j8:
     # ALLOCATE STACK SPACE
     subq $0x28, %rsp                         # [STACK ALLOCATE 32 BYTES]
     # FUNCTION BODY
-    vxorps %zmm0, %zmm0, %zmm0               # SET SRC REGISTER TO ZERO
-    movq 0x10(%rbp), %r10                    # GATHER INDICES
-    vmovdqu32 (%r10), %zmm1                  # LD C MATRIX INDICES [DSTRYBL:r10]
-    movq %rdx, -0x8(%rbp)                    # [SAVE C MATRIX PTR TO STACK]
+    movq %r8, -0x8(%rbp)                     # [SAVE C MATRIX PTR TO STACK]
     movq %r12, -0x10(%rbp)                   # [SAVE R12 REG TO STACK]
     movq %r13, -0x18(%rbp)                   # [SAVE R13 REG TO STACK]
     movq %r14, -0x20(%rbp)                   # [SAVE R14 REG TO STACK]
@@ -52,22 +49,21 @@ asm_gemm_f32_j8:
     vxorps %zmm8, %zmm8, %zmm8
     vxorps %zmm9, %zmm9, %zmm9
 
-    vmovdqu32 (%r9), %zmm1                   # LD A MATRIX INDICES
     # START MATRIX MULTIPLICATION
     xorl %eax, %eax                          # INDEX i
     movq 0x20(%rbp), %r10                    # ID
+    movq 0x10(%rbp), %r8                     # PAGE OFFSET DATA
 .LOOPBEGIN:
     # LOOP INIT
-    cmpl %edi, %eax                          # TEST i < k
+    cmpl %esi, %eax                          # TEST i < k
     jnb .LOOPEXIT
     # LOOP INIT END
-    kmovw  %r11d, %k1                        # SET MASK
-    vgatherdps (%rsi, %zmm1, 4), %zmm0{%k1}  # LOAD A COL TO REGISTER
-                                             # SET MATRIX B
+    vmovups (%rcx), %zmm0{%k1}               # LOAD A COL TO REGISTER
+    # SET MATRIX B
     movl %eax, %r14d                         # CALLER SAVE
+    movq %r9, %r13                           # MOV BASE PAGE ADDRESS
     lea (%r8, %r10, 0x8), %r12               # OFFSET
     movq (%r12), %r12
-    movq %rcx, %r13                          # MOV BASE PAGE ADDRESS
     addq %r12, %r13                          # CALLABLE ADDRESS
     callq *%r13                              # CALL FUNCTION TO SET MAT B
     movl %r14d, %eax                         # CALLER RESTORE
@@ -80,41 +76,29 @@ asm_gemm_f32_j8:
     vfmadd231ps %zmm0, %zmm26, %zmm7         # z7 += z0 * z26
     vfmadd231ps %zmm0, %zmm25, %zmm8         # z8 += z0 * z25
     vfmadd231ps %zmm0, %zmm24, %zmm9         # z9 += z0 * z24
-    # LOOP CLEANUP
-    lea 0x4(%rsi), %rsi                      # INCREMENT A MATRIX PTR
+    # LOOP CLEANUP                   
+    lea (%rcx, %rdi, 0x4), %rcx              # INCREMENT A MATRIX PTR
     addl $1, %eax                            # i = i + 1
     addl $1, %r10d                           # ID = ID + 1
     jmp .LOOPBEGIN
     # LOOP CLEANUP END
 .LOOPEXIT:
-    vxorps %zmm0, %zmm0, %zmm0               # SET SRC REGISTER TO ZERO
     movq -0x8(%rbp), %rdx                    # [RESTORE C MATRIX PTR TO STACK]
-    # vmovups %zmm2, (%rdx)                    # TEST LINE: REMOVE (STORE C)
-    movq 0x10(%rbp), %r10                    # GATHER INDICES
-    vmovdqu32 (%r10), %zmm1                  # LD C MATRIX INDICES [DSTRYBL:r10]
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm2, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm3, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm4, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm5, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm6, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm7, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm8, (%rdx, %zmm1, 0x4){%k1} 
-    lea 0x4(%rdx), %rdx                      # INCREMENT C MATRIX PTR
-    kmovw  %r11d, %k1                        # SET MASK
-    vscatterdps %zmm9, (%rdx, %zmm1, 0x4){%k1} 
+    vmovups %zmm2, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm3, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm4, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm5, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm6, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm7, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm8, (%rdx){%k1}
+    lea (%rdx, %rdi, 0x4), %rdx              # INCREMENT C MATRIX PTR
+    vmovups %zmm9, (%rdx){%k1}
     movq $0x1, %rax                          # SET RETURN VALUE
     # FUNCTION BODY ENDS
     # DEALLOCATE STACK SPACE
