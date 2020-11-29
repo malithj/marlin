29668c7451e9826f5c179f38b368553e9b10fa05
Malith <malithjayaweera.d@northeastern.edu>
2020-11-29 17:15:40 -0500

    added Winograd JIT test

--- test/jit/test_jit_winograd.cc
+++ test/jit/test_jit_winograd.cc
@@ -1,71 +1,59 @@
-#include "conv/winograd/asm_wino.h"
+#include <marlin>
+
+#include "../utils/test_utils.h"
+#include "conv/direct/direct_convolver.h"
 #include "gtest/gtest.h"
-#include "jit/wino_jitter.h"
-#include "jit/wino_store.h"
 
 TEST(JIT, Winograd) {
-  const index_t in_tile_area = 16;
-  const index_t elements_per_tile = 8;
-  const index_t reg_width = 16;
+  Winograd<float, WINO_K_3x3, WINO_O_2x2> winograd;
+  std::shared_ptr<Tensor<float>> input = std::make_shared<Tensor<float>>();
+  std::shared_ptr<Tensor<float>> filter = std::make_shared<Tensor<float>>(true);
+  std::shared_ptr<Tensor<float>> output = std::make_shared<Tensor<float>>();
+
+  const index_t batch = 2;
   const index_t in_channels = 3;
-  const index_t out_channels = 2;
-  const index_t b_stride = in_channels * out_channels;
+  const index_t in_height = 20;
+  const index_t in_width = 20;
+  const index_t out_channels = 10;
+  const index_t filter_height = 3;
+  const index_t filter_width = 3;
 
-  float *b = static_cast<float *>(
-      aligned_alloc(in_tile_area * in_channels * out_channels, sizeof(float)));
+  input->resize({batch, in_channels, in_height, in_width});
+  filter->resize({out_channels, in_channels, filter_height, filter_width});
+  initialize_tensor(input);
+  initialize_tensor(filter);
 
-  for (index_t i = 0; i < in_tile_area * in_channels * out_channels; ++i)
-    b[i] = i;
+#ifdef ENABLE_JIT
+  const index_t in_tile_area = 16;
+  std::shared_ptr<Tensor<float>> transformed_filter =
+      std::make_shared<Tensor<float>>();
+  transformed_filter->resize({out_channels, in_channels, in_tile_area});
+  float *transformed_filter_data = transformed_filter->mutable_data();
+  const float *filter_data = filter->data();
 
-  float *buffer_out = static_cast<float *>(
-      aligned_alloc(elements_per_tile * reg_width, sizeof(float)));
+  winograd.transform_kernel(filter_data, out_channels, in_channels,
+                            transformed_filter_data);
 
-  WinoJitter<float> jitter;
-  jitter.generate_code(b, in_tile_area, in_channels, out_channels);
+  std::shared_ptr<WinoJitter<float>> jitter =
+      std::make_shared<WinoJitter<float>>();
+  jitter->generate_code(transformed_filter_data, in_tile_area, in_channels,
+                        out_channels);
+#endif
 
-  index_t idx = 0;
+#ifdef ENABLE_JIT
+  winograd.run(input, filter, output, jitter);
+#else
+  winograd.run(input, filter, output);
+#endif
 
-  for (index_t m = 0; m < out_channels; ++m) {
-    for (index_t tidx = 0; tidx < in_tile_area; tidx += elements_per_tile) {
-      for (index_t c = 0; c < in_channels; ++c) {
-        jitter.execute(idx);
+  std::shared_ptr<Tensor<float>> output_d = std::make_shared<Tensor<float>>();
+  std::unique_ptr<conv2d::DirectConvolver<float>> direct_convolver =
+      std::make_unique<conv2d::DirectConvolver<float>>();
+  std::vector<index_t> p = {0, 0, 0, 0};
+  std::vector<index_t> s = {1, 1};
+  direct_convolver->set_padding(p);
+  direct_convolver->set_stride(s);
+  direct_convolver->run(filter.get(), input.get(), output_d.get());
 
-        // Copy results to the modified array from the original array
-        // using inline assembly.
-        // Do not touch, if you don't absolutely know what you are doing.
-        //
-        __asm__ __volatile__(
-            "movq	%[reg], %%rax\n\t"
-            "vmovaps %%zmm31, (%%rax)\n\t"
-            "addq	$64, %%rax\n\t"
-            "vmovaps %%zmm30, (%%rax)\n\t"
-            "addq	$64, %%rax\n\t"
-            "vmovaps %%zmm29, (%%rax)\n\t"
-            "addq	$64, %%rax\n\t"
-            "vmovaps %%zmm28, (%%rax)\n\t"
-            "addq	$64, %%rax\n\t"
-            "vmovaps %%zmm27, (%%rax)\n\t"
-            "addq	$64, %%rax\n\t"
-            "vmovaps %%zmm26, (%%rax)\n\t"
-            "addq	$64, %%rax\n\t"
-            "vmovaps %%zmm25, (%%rax)\n\t"
-            "addq	$64, %%rax\n\t"
-            "vmovaps %%zmm24, (%%rax)\n\t"
-            :
-            : [ reg ] "m"(buffer_out)
-            :);
-
-        const float *b_ptr = b + tidx * b_stride + m * in_channels + c;
-        for (index_t h = 0; h < elements_per_tile; ++h) {
-          for (index_t w = 0; w < reg_width; ++w) {
-            float value = b_ptr[h * b_stride];
-            EXPECT_EQ(value, buffer_out[h * reg_width + w]);
-          }
-        }
-        ++idx;
-      }
-    }
-  }
-  aligned_free(buffer_out);
-  aligned_free(b);
+  verify_execution(output_d.get(), output.get());
 }
