0b134573c40d6b98ec510d62ca7fd3bd41ffe0ed
Malith <malithjayaweera.d@northeastern.edu>
2021-02-25 09:58:24 -0500

    added check to verify the availability of AVX512

--- test/main.cc
+++ test/main.cc
@@ -8,6 +8,7 @@
  ******************************************************************************/
 /* Malith Jayaweera
 *******************************************************************************/
+#include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -24,9 +25,19 @@ int main(int argc, char *argv[]) {
   setenv("CPP_MINIMUM_LOG_LEVEL", "5", 1);
   LOG_INFO("starting the test suite");
   testing::InitGoogleTest(&argc, argv);
-#ifndef __AVX512F__
-  std::cout << "AVX512F not available on the target architecture" << std::endl;
-  return 0;
+#if defined(__GNUC__)
+  __builtin_cpu_init();
+  if (!__builtin_cpu_supports("avx512f")) {
+    std::cout << "AVX512F not available on the target architecture"
+              << std::endl;
+    return 0;
+  }
+#elif defined(__INTEL_COMPILER)
+  if (!_may_i_use_cpu_feature(_FEATURE_AVX512F)) {
+    std::cout << "AVX512F not available on the target architecture"
+              << std::endl;
+    return 0;
+  }
 #endif
   int result = RUN_ALL_TESTS();
   delete Env<void>::get_env();
